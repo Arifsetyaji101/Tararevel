8d5c193dfea3bc7f1f8bd441091695c5fcb02598
Arifsetyaji101 <Arifsetyaji101@gmail.com>
2024-03-12 05:16:49 +0700

    fix: api/asset.php

--- api/asset.php
+++ api/asset.php
@@ -6,8 +6,14 @@
  */
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    echo require __DIR__ . '/../public/frontend/styles' . basename($_GET['file']);
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    echo require __DIR__ . '/../public/frontend/scripts/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'css') {
+    header("Content-type: text/css; charset: UTF-8");
+    echo require __DIR__ . '/../public/backend/css/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'js') {
+    header('Content-Type: application/javascript; charset: UTF-8');
+    echo require __DIR__ . '/../public/backend/js/' . basename($_GET['file']);
 }
